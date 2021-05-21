04aacfa417dd29d0c6ab1d1359aee645ad9495ce
Mateus Alves <mateus.alves.unb@gmail.com>
2021-05-21 19:25:16 -0300

    I was actually right at first D:. The  world is full of this kind of stuff

--- muryoukuusho.sh
+++ muryoukuusho.sh
@@ -1,14 +1,6 @@
 echo "Updating and installing essential tools."
-sudo apt update && sudo apt -y upgrade && sudo apt autoremove
+sudo apt update && sudo apt -y upgrade && sudo apt -y autoremove
 sudo apt install -y build-essential libssl-dev libffi-dev python3-dev python3-pip
-
-echo "Installing oh my zsh."
-sudo rm -r ~/.oh-my-zsh
-sudo apt install -y zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-exit
-sudo mv ~/.zshrc ~/.zshrc.backup
-sudo cp .zshrc ~/.zshrc
 
 echo "Installing tmux."
 sudo apt install -y tmux
@@ -41,5 +33,12 @@ sudo snap install --classic ccls
 python3 -m pip install pynvim
 
 cp coc-settings.json ~/.config/nvim/coc-settings.json
+
+echo "Installing oh my zsh."
+sudo rm -r ~/.oh-my-zsh
+sudo mv ~/.zshrc ~/.zshrc.backup
+sudo cp .zshrc ~/.zshrc
+sudo apt install -y zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "Done."
