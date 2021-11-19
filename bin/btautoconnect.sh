e4b6452aee55c4866ab7793d66d51b2ff0b06d3c
Mateus Alves <mateus.alves.unb@gmail.com>
2021-11-19 08:32:31 -0300

    Update btautoconnect.sh

--- bin/btautoconnect.sh
+++ bin/btautoconnect.sh
@@ -4,7 +4,7 @@ result=1
 while [ $result -ne 0 ]; do
     rfkill unblock all
     sleep 1
-    bluetoothctl connect DC:2C:26:F6:F3:98
+    bluetoothctl connect DC:2C:26:F6:F3:99
     result=$?
     sleep 2
 done
