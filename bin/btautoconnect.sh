3a34f7baae67ccf6c070d28386b166788f1b08d7
Mateus Alves <mateus.alves.unb@gmail.com>
2021-11-22 19:01:24 -0300

    Updating btautoconnect.sh

--- bin/btautoconnect.sh
+++ bin/btautoconnect.sh
@@ -4,7 +4,14 @@ result=1
 while [ $result -ne 0 ]; do
     rfkill unblock all
     sleep 1
-    bluetoothctl connect DC:2C:26:F6:F3:99
+    bluetoothctl connect DC:2C:26:F6:F3:99 &> /dev/null
     result=$?
     sleep 2
+
+    if [ $result -eq 0 ]; then
+        setxkbmap -model pc105 -layout us -variant intl &
+        echo ""
+    else
+        echo ""
+    fi
 done
